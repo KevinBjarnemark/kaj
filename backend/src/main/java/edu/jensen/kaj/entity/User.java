ded64227881e8f96971db9cbfa0a477109efb704
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2026-02-17 11:30:04 +0100

    fix: change name to username for user object

--- backend/src/main/java/edu/jensen/kaj/entity/User.java
+++ backend/src/main/java/edu/jensen/kaj/entity/User.java
@@ -59,12 +59,12 @@ public class User {
         this.email = email;
     }
 
-    public String getUsername() {
-        return username;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public void setName(String username) {
-        this.username = username;
+    public String getUsername() {
+        return username;
     }
 
     public LocalDateTime getCreatedAt() {
